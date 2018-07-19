f82d11dfc825539c143f55b8653b5d8cd1f6c785
김흥열 <favianred@gmail.com>
2018-07-19 18:35:39 +0900

    * codility   * PermCheck

---
+++ src/codility/PermCheck/Solution.java
@@ -0,0 +1,39 @@
+package codility.PermCheck;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class Solution {
+    public int solution(int[] A) {
+        // write your code in Java SE 8
+        int[] B = new int[A.length];
+        for (int n : A) {
+            if (n <= A.length) {
+                B[n - 1] = 1;
+            }
+        }
+
+        int isPermutation = 1;
+        for (int i = 0; i < B.length; i++) {
+            if (B[i] == 0) {
+                isPermutation = 0;
+                break;
+            }
+        }
+
+        return isPermutation;
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        int[] list = {4, 1, 3};
+        Solution solution = new Solution();
+        int result = solution.solution(list);
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write(String.valueOf(result));
+        bw.flush();
+        bw.close();
+    }
+}

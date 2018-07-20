4d2a9172ae5c00318ec66d1da7c72478b15af2c0
김흥열 <favianred@gmail.com>
2018-07-20 16:30:43 +0900

    * datastructure   * ch2. 재귀

---
+++ src/datastructure/HanoiTowerSolu.java
@@ -0,0 +1,17 @@
+package datastructure;
+
+public class HanoiTowerSolu {
+    public static void main(String[] args) {
+        hanoiTowerMove(3, 'A', 'B', 'C');
+    }
+
+    private static void hanoiTowerMove(int num, char from, char by, char to) {
+        if (num == 1) {
+            System.out.printf("원반 1을 %c에서 %c로 이동 \n", from, to);
+        } else {
+            hanoiTowerMove(num - 1, from, to, by);
+            System.out.printf("원반 %d을(를) %c에서 %c로 이동 \n", num, from, to);
+            hanoiTowerMove(num - 1, by, from, to);
+        }
+    }
+}

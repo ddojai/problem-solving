d3af4b88786005da6b3b67302c0652a12f739c7f
김흥열 <favianred@gmail.com>
2018-07-21 19:31:35 +0900

    * hackerrank   * ctci string_making_anagrams

--- src/hackerrank/tut/ctci/string_making_anagrams/Solution.java
+++ src/hackerrank/tut/ctci/string_making_anagrams/Solution.java
@@ -12,9 +12,7 @@ public class Solution {
 
     // Complete the makeAnagram function below.
     static int makeAnagram(String a, String b) {
-        /*
         Map<Character, Integer> mapA = new HashMap<>();
-        Map<Character, Integer> mapB = new HashMap<>();
         for (int i = 0; i < a.length(); i++)
         {
             char cA = a.charAt(i);
@@ -23,7 +21,11 @@ public class Solution {
             } else {
                 mapA.put(cA, mapA.get(cA) + 1);
             }
+        }
 
+        Map<Character, Integer> mapB = new HashMap<>();
+        for (int i = 0; i < b.length(); i++)
+        {
             char cB = b.charAt(i);
             if (mapB.get(cB) == null) {
                 mapB.put(cB, 1);
@@ -31,15 +33,30 @@ public class Solution {
                 mapB.put(cB, mapB.get(cB) + 1);
             }
         }
-         */
-        return 1;
+
+        int sum = 0;
+        for (char c = 'a'; c <= 'z'; c++) {
+            int cntA = 0;
+            if (mapA.get(c) != null) {
+                cntA = mapA.get(c);
+            }
+
+            int cntB = 0;
+            if (mapB.get(c) != null) {
+                cntB = mapB.get(c);
+            }
+
+            sum += Math.abs(cntA - cntB);
+        }
+
+        return sum;
     }
 
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) throws IOException {
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
-
+//        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
         String a = scanner.nextLine();
 
         String b = scanner.nextLine();

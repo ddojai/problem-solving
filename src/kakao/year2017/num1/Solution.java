f1dd5d7a0485287a0683cd5b3e8292c5be68b874
김흥열 <favianred@gmail.com>
2018-09-11 15:51:42 +0900

    * 카카오   * 2017년도 문제 1

--- src/kakao/year2017/num1/Solution.java
+++ src/kakao/year2017/num1/Solution.java
@@ -1,54 +1,42 @@
 package kakao.year2017.num1;
 
-import java.io.*;
-import java.util.stream.Stream;
-
 /**
- * 30 min
+ * 코드 출처
+ * http://javaplant.tistory.com/4?category=784551
  */
 public class Solution {
     public static void main(String[] args) {
-        try {
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-            int n = Integer.parseInt(br.readLine());
-            String strArr1 = br.readLine();
-            String strArr2 = br.readLine();
+        int[] arr1;
+        int[] arr2;
 
-            int[] arr1 = Stream.of(strArr1.trim().split(",")).mapToInt(Integer::parseInt).toArray();
-            int[] arr2 = Stream.of(strArr2.trim().split(",")).mapToInt(Integer::parseInt).toArray();
+        arr1 = new int[]{9, 20, 28, 18, 11};
+        arr2 = new int[]{30, 1, 21, 17, 28};
+        printArr(decodeScreetMap(5, arr1, arr2));
 
-            for (int i = 0; i < arr1.length; i++) {
-                int a = arr1[i];
-                int b = arr2[i];
-                int c = a | b;
-                String binaryString = Integer.toBinaryString(c);
-                for (int j = 0; j < binaryString.length(); j++) {
-                    if (binaryString.charAt(j) == '1') {
-                        bw.write("#");
-                    } else {
-                        bw.write(" ");
-                    }
+        arr1 = new int[]{46, 33, 33, 22, 31, 50};
+        arr2 = new int[]{27, 56, 19, 14, 14, 10};
+        printArr(decodeScreetMap(6, arr1, arr2));
+    }
+
+    private static String[] decodeScreetMap(int n, int[] arr1, int[] arr2) {
+        String[] retArr = new String[n];
+        for(int i = 0; i < n; i++) {
+            StringBuilder binaryString = new StringBuilder(Integer.toBinaryString(arr1[i]|arr2[i]));
+            if (binaryString.length() < n) {
+                for (int j = 0; j < n - binaryString.length(); j++) {
+                    binaryString.append(0);
                 }
-                bw.write("\n");
             }
-            bw.flush();
-            br.close();
-            bw.close();
-        } catch (IOException e) {
-            e.printStackTrace();
+            retArr[i] = binaryString.toString().replace("0", " ").replace("1", "#");
         }
+        return retArr;
+    }
+
+    private static void printArr(String[] arr) {
+        StringBuilder sb = new StringBuilder();
+        for(String str: arr) {
+            sb.append(", \""+str+"\"");
+        }
+        System.out.println(sb.toString().replaceFirst(", ", ""));
     }
 }
-
-/*
-5
-9,20,28,18,11
-30,1,21,17,28
-*/
-
-/*
-6
-46,33,33,22,31,50
-27,56,19,14,14,10
-*/

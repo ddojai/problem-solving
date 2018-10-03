81f50cf74726673cb4a6e39db5fd45200c62fde8
김흥열 <favianred@gmail.com>
2018-10-03 23:01:42 +0900

    * 백준 강의   * 알고리즘 기초     * 그대로 출력하기2

---
+++ src/baekjoon/lecture/beginner/io/p11719/Main.java
@@ -0,0 +1,12 @@
+package baekjoon.lecture.beginner.io.p11719;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextLine()){
+            System.out.println(scanner.nextLine());
+        }
+    }
+}

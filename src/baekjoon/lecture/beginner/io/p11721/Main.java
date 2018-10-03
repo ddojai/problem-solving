53944eda899e79ead42180234b98452640246005
김흥열 <favianred@gmail.com>
2018-10-03 23:11:52 +0900

    * 백준 강의   * 알고리즘 기초     * 열 개씩 끊어 출력하기

---
+++ src/baekjoon/lecture/beginner/io/p11721/Main.java
@@ -0,0 +1,27 @@
+package baekjoon.lecture.beginner.io.p11721;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        int count = input.length() / 10;
+        for (int i = 0; i < count; i++) {
+            int startIndex = i * 10;
+            int endIndex = startIndex + 10;
+            String subString = input.substring(startIndex, endIndex);
+            System.out.println(subString);
+        }
+
+        int etc = input.length() % 10;
+        if (etc != 0)
+        {
+            int startIndex = count * 10;
+            String subString = input.substring(startIndex);
+            System.out.println(subString);
+        }
+    }
+}

3d56e9e9560252145982f466468b256500190a43
김흥열 <favianred@gmail.com>
2018-10-07 13:39:40 +0900

    * 백준 강의   * 알고리즘 기초 (자료구조1 큐)     * 큐

--- src/baekjoon/lecture/beginner/datastructure1/queue/p10845/Main.java
+++ src/baekjoon/lecture/beginner/datastructure1/queue/p10845/Main.java
@@ -1,18 +1,10 @@
-package baekjoon.queue.p10845;
+package baekjoon.lecture.beginner.datastructure1.queue.p10845;
 
 import java.io.*;
 import java.util.*;
 
 /**
- * 10845번 큐
- * 정수를 저장하는 큐를 구현한 다음, 입력으로 주어지는 명령을 처리하는 프로그램을 작성하시오.
- * 명령은 총 여섯 가지이다.
- * push X: 정수 X를 큐에 넣는 연산이다.
- * pop: 큐에서 가장 앞에 있는 정수를 빼고, 그 수를 출력한다. 만약 큐에 들어있는 정수가 없는 경우에는 -1을 출력한다.
- * size: 큐에 들어있는 정수의 개수를 출력한다.
- * empty: 큐가 비어있으면 1, 아니면 0을 출력한다.
- * front: 큐의 가장 앞에 있는 정수를 출력한다. 만약 큐에 들어있는 정수가 없는 경우에는 -1을 출력한다.
- * back: 큐의 가장 뒤에 있는 정수를 출력한다. 만약 큐에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+ * https://www.acmicpc.net/problem/10845
  */
 class Main {
     public static void main(String[] args) {
@@ -21,14 +13,10 @@ class Main {
             BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
             int count = Integer.parseInt(br.readLine());
-            List<String> inputList = new ArrayList<>();
-            for (int i = 0; i < count; i++) {
-                inputList.add(br.readLine());
-            }
 
             LinkedList<Integer> queue = new LinkedList<>();
-
-            for (String input : inputList) {
+            for (int i = 0; i < count; i++) {
+                String input = br.readLine();
                 String[] split = input.split(" ");
                 switch (split[0]) {
                     case "push":

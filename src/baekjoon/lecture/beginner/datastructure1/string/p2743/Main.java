36cf4b612e1c89f0836aba5bacd9393f451289eb
김흥열 <favianred@gmail.com>
2018-10-07 14:31:32 +0900

    * 백준 강의   * 알고리즘 기초 (자료구조1 문자열)     * 단어 길이 재기

---
+++ src/baekjoon/lecture/beginner/datastructure1/string/p2743/Main.java
@@ -0,0 +1,11 @@
+package baekjoon.lecture.beginner.datastructure1.string.p2743;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        System.out.println(input.length());
+    }
+}

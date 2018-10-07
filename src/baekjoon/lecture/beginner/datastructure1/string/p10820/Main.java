b2328ad2680eb36e135fbfdb2c08ba7b7928263a
김흥열 <favianred@gmail.com>
2018-10-07 14:28:41 +0900

    * 백준 강의   * 알고리즘 기초 (자료구조1 문자열)     * 문자열 분석

--- src/baekjoon/lecture/beginner/datastructure1/string/p10820/Main.java
+++ src/baekjoon/lecture/beginner/datastructure1/string/p10820/Main.java
@@ -7,25 +7,24 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         while (scanner.hasNextLine()) {
             String input = scanner.nextLine();
-            int cntLower = 0;
-            int cntUpper = 0;
-            int cntNum = 0;
-            int cntBlank = 0;
+            int lower = 0;
+            int upper = 0;
+            int digit = 0;
+            int space = 0;
             for (int i = 0; i < input.length(); i++) {
                 char c = input.charAt(i);
 
-                if (c >= 'a' && c <= 'z') {
-                    cntLower++;
-                } else if (c >= 'A' && c <= 'Z') {
-                    cntUpper++;
-                } else if (c >= '0' && c <= '9') {
-                    cntNum++;
-                } else {
-                    cntBlank++;
+                if ('a' <= c && c <= 'z') {
+                    lower++;
+                } else if ('A' <= c && c <= 'Z') {
+                    upper++;
+                } else if ('0' <= c && c <= '9') {
+                    digit++;
+                } else if (c == ' '){
+                    space++;
                 }
             }
-            System.out.print(cntLower + " " + cntUpper + " " + cntNum + " " + cntBlank);
-            System.out.println();
+            System.out.println(lower + " " + upper + " " + digit + " " + space);
         }
     }
 }

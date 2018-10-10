9354860dc5dda724ce154c8260f3ea36e190b77d
김흥열 <favianred@gmail.com>
2018-10-10 16:23:27 +0900

    * 백준 강의   * 알고리즘 기초 (수학1)     * 나머지

---
+++ src/baekjoon/lecture/beginner/math1/p10430/Main.java
@@ -0,0 +1,20 @@
+package baekjoon.lecture.beginner.math1.p10430;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        String numList[] = input.split(" ");
+        int a = Integer.parseInt(numList[0]);
+        int b = Integer.parseInt(numList[1]);
+        int c = Integer.parseInt(numList[2]);
+
+        System.out.println((a + b) % c);
+        System.out.println((a % c + b % c) % c);
+        System.out.println((a * b) % c);
+        System.out.println((a % c * b % c) % c);
+    }
+}

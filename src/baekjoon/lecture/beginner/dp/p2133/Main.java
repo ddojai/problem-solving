5ed051d36bbaee8722e4e7dbf00c350efcd951fa
김흥열 <favianred@gmail.com>
2018-10-10 14:48:06 +0900

    * 백준 강의   * 알고리즘 기초 (다이나믹 프로그래밍1)     * 타일 채우기

---
+++ src/baekjoon/lecture/beginner/dp/p2133/Main.java
@@ -0,0 +1,22 @@
+package baekjoon.lecture.beginner.dp.p2133;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        long[] d = new long[n + 1];
+        d[0] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            d[i] = d[i - 2] * 3;
+            for (int j = i - 4; j >= 0; j-=2) {
+                d[i] += d[j] * 2;
+            }
+        }
+
+        System.out.println(d[n]);
+    }
+}

a9531dac5e57df98591ffb8e38edc6fbf35f1fc9
김흥열 <favianred@gmail.com>
2018-10-10 17:11:52 +0900

    * 백준 강의   * 알고리즘 기초 (수학1)     * 최소공배수

---
+++ src/baekjoon/lecture/beginner/math1/p1934/Main.java
@@ -0,0 +1,24 @@
+package baekjoon.lecture.beginner.math1.p1934;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
+        while (t-- > 0) {
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
+
+            System.out.println(a * b / gcd(a, b));
+        }
+    }
+
+    private static int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+
+        return gcd(b, a % b);
+    }
+}

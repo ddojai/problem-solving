7db1e6a35375eefafeb3f16189bc735e4dc6204f
김흥열 <favianred@gmail.com>
2018-10-12 17:31:26 +0900

    * 백준 강의   * 알고리즘 기초 (수학1)     * 소수 찾기 (p1978)

--- src/baekjoon/lecture/beginner/math1/base_conversion/p11576/Main.java
+++ src/baekjoon/lecture/beginner/math1/base_conversion/p11576/Main.java
@@ -7,16 +7,17 @@ public class Main {
         if (num == 0) {
             return;
         }
-        convert(num/base, base);
-        System.out.print(num%base + " ");
+        convert(num / base, base);
+        System.out.print(num % base + " ");
     }
+
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
         int n = sc.nextInt();
         int ans = 0;
-        for (int i = 0; i<n; i++) {
+        for (int i = 0; i < n; i++) {
             int x = sc.nextInt();
             ans = ans * a + x;
         }

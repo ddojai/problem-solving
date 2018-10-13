9ebceaf3a9a7fd24e8f77cbd8559f5a8b942edf3
김흥열 <favianred@gmail.com>
2018-10-13 19:48:21 +0900

    * 백준 강의   * 알고리즘 기초 (정렬)     * 버블소트

---
+++ src/baekjoon/lecture/beginner/sort/p1377/Main.java
@@ -0,0 +1,53 @@
+package baekjoon.lecture.beginner.sort.p1377;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        List<Pair> list = new ArrayList<>(n);
+        for (int i = 0; i < n; i++) {
+            list.add(new Pair(Integer.parseInt(br.readLine()), i));
+        }
+        br.close();
+
+        Collections.sort(list);
+
+        int result = 0;
+        for (int i = 0; i < n; i++) {
+            Pair pair = list.get(i);
+            int diff = pair.getIdx() - i;
+            if (diff > result) {
+                result = diff;
+            }
+        }
+
+        System.out.println(result + 1);
+    }
+}
+
+class Pair implements Comparable<Pair>{
+    private int var;
+    private int idx;
+
+    Pair(int var, int idx) {
+        this.var = var;
+        this.idx = idx;
+    }
+
+    public int getIdx() {
+        return idx;
+    }
+
+    @Override
+    public int compareTo(Pair o) {
+        return var - o.var;
+    }
+}

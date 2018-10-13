3f7504cdab82a1314c52c52b1672bf099f0672a5
김흥열 <favianred@gmail.com>
2018-10-13 20:04:48 +0900

    * 백준 강의   * 알고리즘 기초 (정렬)     * K번째 수

---
+++ src/baekjoon/lecture/beginner/sort/p11004/Main.java
@@ -0,0 +1,28 @@
+package baekjoon.lecture.beginner.sort.p11004;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] split = br.readLine().split(" ");
+        int n = Integer.parseInt(split[0]);
+        int k = Integer.parseInt(split[1]);
+
+        StringTokenizer stringTokenizer = new StringTokenizer(br.readLine(), " ");
+        List<Integer> list = new ArrayList<>(n);
+        for (int i = 0; i < n; i++) {
+            list.add(Integer.parseInt(stringTokenizer.nextToken()));
+        }
+
+        Collections.sort(list);
+
+        System.out.println(list.get(k-1));
+    }
+}

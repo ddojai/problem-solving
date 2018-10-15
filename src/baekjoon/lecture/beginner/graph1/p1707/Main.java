efbc340bcc48a7ecb72cac4e36dfd5046241c58f
김흥열 <favianred@gmail.com>
2018-10-15 18:31:47 +0900

    * 백준 강의   * 알고리즘 기초 (그래프1)     * 이분 그래프

---
+++ src/baekjoon/lecture/beginner/graph1/p1707/Main.java
@@ -0,0 +1,63 @@
+package baekjoon.lecture.beginner.graph1.p1707;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class Main {
+
+    private static void dfs(ArrayList<Integer>[] edge, int[] color, int v, int c) {
+        color[v] = c;
+        for (int next : edge[v]) {
+            if (color[next] == 0) {
+                dfs(edge, color, next, 3 - c);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+        while (t-- > 0) {
+            String[] split = br.readLine().split(" ");
+            int v = Integer.parseInt(split[0]);
+            int e = Integer.parseInt(split[1]);
+
+            ArrayList<Integer>[] edge = (ArrayList<Integer>[]) new ArrayList[v + 1];
+            for (int i = 1; i <= v; i++) {
+                edge[i] = new ArrayList<>();
+            }
+
+            for (int i = 0; i < e; i++) {
+                split = br.readLine().split(" ");
+                int a = Integer.parseInt(split[0]);
+                int b = Integer.parseInt(split[1]);
+                edge[a].add(b);
+                edge[b].add(a);
+            }
+
+            int[] color = new int[v + 1];
+
+            for (int i = 1; i <= v; i++) {
+                if (color[i] == 0) {
+                    dfs(edge, color, i, 1);
+                }
+            }
+
+            boolean isBipartite = true;
+            breakOut:
+            for (int i = 1; i <= v; i++) {
+                for (int next : edge[i]) {
+                    if (color[i] == color[next]) {
+                        isBipartite = false;
+                        break breakOut;
+                    }
+                }
+            }
+            System.out.println(isBipartite ? "YES" : "NO");
+        }
+        br.close();
+    }
+}

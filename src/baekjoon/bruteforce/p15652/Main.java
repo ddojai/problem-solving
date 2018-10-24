0668fae3965d41a79912d140d944a63521c31d84
김흥열 <favianred@gmail.com>
2018-10-24 17:04:48 +0900

    * 백준 강의   * 2018 SW 역량 테스트 준비 - 기초     * 브루트 포스 (비트마스크)       * N 과 M 4

--- src/baekjoon/bruteforce/p15652/Main.java
+++ src/baekjoon/bruteforce/p15652/Main.java
@@ -3,29 +3,36 @@ package baekjoon.bruteforce.p15652;
 import java.util.Scanner;
 
 public class Main {
-    private static boolean[] c = new boolean[10];
-    private static int[] a = new int[10];
+
+    private static int[] cnt = new int[10];
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
         int m = scanner.nextInt();
 
-        go(0, 1, n, m);
+        go(1, 0, n, m);
     }
 
-    private static void go(int idx, int start, int n, int m) {
-        if (idx == m) {
+    private static void go(int index, int selected, int n, int m) {
+        if (selected == m) {
             StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < m; i++) {
-                sb.append(a[i]).append(" ");
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= cnt[i]; j++) {
+                    sb.append(i);
+                    sb.append(" ");
+                }
             }
             System.out.println(sb);
             return;
         }
-        for (int i = start; i <= n; i++) {
-            a[idx] = i;
-            go(idx + 1, i, n, m);
+
+        if (index > n) return;
+        for (int i = m - selected; i >= 1; i--) {
+            cnt[index] = i;
+            go(index + 1, selected + i, n, m);
         }
+        cnt[index] = 0;
+        go(index + 1, selected, n, m);
     }
 }

64c057d203b3c6b2a4e0b54a41f4c053f265bba4
ddojai <favianred@gmail.com>
2019-08-04 16:30:11 +0900

    * leetcode algorithm   * palindrome_number   * rename two_numbers to two_sum

---
+++ src/leetcode/problems/two_sum/Solution.kt
@@ -0,0 +1,22 @@
+package leetcode.problems.two_sum
+
+import kotlin.test.assertTrue
+
+class Solution {
+    fun twoSum(nums: IntArray, target: Int): IntArray {
+        lateinit var arr: IntArray
+        for (i in 0..nums.size - 2) {
+            for (j in i + 1 until nums.size)
+                if (nums[i] + nums[j] == target) {
+                    arr = intArrayOf(i, j)
+                }
+        }
+        return arr
+    }
+}
+
+fun main(args: Array<String>) {
+    val sol = Solution()
+    val result = sol.twoSum(intArrayOf(2, 7, 11, 15), 9)
+    assertTrue(intArrayOf(0, 1).contentEquals(result))
+}

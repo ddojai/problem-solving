308726963a66880bf634cabe9da4570b832e3a56
ddojai <favianred@gmail.com>
2019-08-27 11:55:21 +0900

    * leetcode   * removeElement main

--- src/main/java/leetcode/problems/removeElement/Solution.java
+++ src/main/java/leetcode/problems/removeElement/Solution.java
@@ -15,4 +15,16 @@ public class Solution {
         }
         return n;
     }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        int[] nums = {0, 1, 2, 2, 3, 0, 4, 2};
+        int val = 2;
+        int len = solution.removeElement(nums, val);
+
+        for (int i = 0; i < len; i++) {
+            System.out.println(nums[i]);
+        }
+    }
 }

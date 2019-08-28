25bcb7455461fe204571d997ab084ee1ea98c4ab
ddojai <favianred@gmail.com>
2019-08-29 01:13:56 +0900

    * leetcode   * implementStrstr

---
+++ src/main/java/leetcode/problems/implementStrstr/Solution.java
@@ -0,0 +1,26 @@
+package leetcode.problems.implementStrstr;
+
+public class Solution {
+    public int strStr(String haystack, String needle) {
+        if (needle.equals("")) {
+            return 0;
+        }
+
+        for (int i = 0; i < haystack.length() - needle.length() + 1; i++) {
+            if (haystack.charAt(i) == needle.charAt(0)) {
+                boolean existNeedle = true;
+                for (int j = 0; j < needle.length(); j++) {
+                    if (haystack.charAt(i + j) != needle.charAt(j)) {
+                        existNeedle = false;
+                        break;
+                    }
+                }
+                if (existNeedle) {
+                    return i;
+                }
+            }
+        }
+
+        return -1;
+    }
+}

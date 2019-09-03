ffacca75631aa3bdc7a0f7e1eb7ab38c6ca91ec9
ddojai <favianred@gmail.com>
2019-09-03 23:46:26 +0900

    * leetcode   * maximumSubarray

---
+++ src/main/java/leetcode/problems/maximumSubarray/Solution.java
@@ -0,0 +1,19 @@
+package leetcode.problems.maximumSubarray;
+
+public class Solution {
+    public int maxSubArray(int[] nums) {
+        int sum = nums[0];
+        int max = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            sum += nums[i];
+            if (nums[i] > sum) {
+                sum = nums[i];
+            }
+
+            if (max < sum) {
+                max = sum;
+            }
+        }
+        return max;
+    }
+}

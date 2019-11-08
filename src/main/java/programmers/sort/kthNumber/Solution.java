7291abbf0c5c1f22637c2d43ba9498d314c42578
ddojai <favianred@gmail.com>
2019-11-09 01:48:03 +0900

    * programmers   * k번째 수

---
+++ src/main/java/programmers/sort/kthNumber/Solution.java
@@ -0,0 +1,21 @@
+package programmers.sort.kthNumber;
+
+import java.util.Arrays;
+
+/**
+ * https://programmers.co.kr/learn/courses/30/lessons/42748?language=java
+ */
+public class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+        for (int n = 0; n < commands.length; n++) {
+            int i = commands[n][0];
+            int j = commands[n][1];
+            int k = commands[n][2];
+            int[] arr = Arrays.copyOfRange(array, i - 1, j);
+            Arrays.sort(arr);
+            answer[n] = arr[k - 1];
+        }
+        return answer;
+    }
+}

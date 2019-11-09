f3622021796310dfabad541658827343d739042d
ddojai <favianred@gmail.com>
2019-11-09 14:02:04 +0900

    * programmers   * 완전탐색-모의고사   * https://programmers.co.kr/learn/courses/30/lessons/42840

---
+++ src/main/java/programmers/courses30_42840/Solution.java
@@ -0,0 +1,49 @@
+package programmers.courses30_42840;
+
+import java.util.ArrayList;
+
+public class Solution {
+    public int[] solution(int[] answers) {
+        int countA = 0;
+        int countB = 0;
+        int countC = 0;
+
+        int[] a = {1, 2, 3, 4, 5};
+        int[] b = {2, 1, 2, 3, 2, 4, 2, 5};
+        int[] c = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+        for (int i = 0; i < answers.length; i++) {
+            int indexA = i % 5;
+            int indexB = i % 8;
+            int indexC = i % 10;
+            int answer = answers[i];
+            if (a[indexA] == answer) {
+                countA++;
+            }
+            if (b[indexB] == answer) {
+                countB++;
+            }
+            if (c[indexC] == answer) {
+                countC++;
+            }
+        }
+
+        ArrayList<Integer> list = new ArrayList<>();
+        if (countA >= countB && countA >= countC) {
+            list.add(1);
+        }
+        if (countB >= countA && countB >= countC) {
+            list.add(2);
+        }
+        if (countC >= countA && countC >= countB) {
+            list.add(3);
+        }
+
+
+        int[] answer = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i);
+        }
+
+        return answer;
+    }
+}

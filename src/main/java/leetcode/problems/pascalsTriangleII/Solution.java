8470e2935a7f8b0f507748535a272d241bae7dd9
ddojai <favianred@gmail.com>
2020-04-09 02:36:43 +0900

    * leetcode   * https://leetcode.com/problems/pascals-triangle-ii/     * x

---
+++ src/main/java/leetcode/problems/pascalsTriangleII/Solution.java
@@ -0,0 +1,20 @@
+package leetcode.problems.pascalsTriangleII;
+
+import java.util.*;
+
+public class Solution {
+  public List<Integer> getRow(int rowIndex) {
+    if (rowIndex == 0)
+      return new LinkedList<>(Collections.singletonList(1));
+
+    List<Integer> prevRow = getRow(rowIndex - 1);
+    List<Integer> currRow = new LinkedList<>(Collections.singletonList(1));
+
+    for (int i = 0; i < prevRow.size() - 1; i++) {
+      currRow.add(prevRow.get(i) + prevRow.get(i + 1));
+    }
+    currRow.add(1);
+
+    return currRow;
+  }
+}

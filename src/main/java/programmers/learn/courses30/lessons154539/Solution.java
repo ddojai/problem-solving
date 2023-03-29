aa75376d11665f09ef818006b4ee3f1d2d19a815
ddojai <favianred@gmail.com>
2023-03-29 23:00:38 +0900

    Programmers > 코딩테스트 연습 > 연습문제 >뒤에 있는 큰 수 찾기
    블로그 해답 : https://ifuwanna.tistory.com/231

--- src/main/java/programmers/learn/courses30/lessons154539/Solution.java
+++ src/main/java/programmers/learn/courses30/lessons154539/Solution.java
@@ -1,25 +1,27 @@
 package programmers.learn.courses30.lessons154539;
 
+import java.util.Stack;
+
 /**
  * https://school.programmers.co.kr/learn/courses/30/lessons/154539
  */
 class Solution {
     public int[] solution(int[] numbers) {
         int[] answer = new int[numbers.length];
+
+        //모든 원소에 대한 뒷 큰수들을 차례로 담은 배열을 return
+        Stack<Integer> stack = new Stack<>();
         for (int i = 0; i < numbers.length; i++) {
-            // numbers에 마지막 값은 j = i + 1 이라 for문 돌지 않음 (뒷 큰수가 없음)
-            for (int j = i + 1; j < numbers.length; j++) {
-                if (numbers[i] < numbers[j]) {
-                    answer[i] = numbers[j];
-                    break;
-                }
+            while (!stack.isEmpty() && numbers[stack.peek()] < numbers[i]) {
+                answer[stack.pop()] = numbers[i];
             }
-            // 1 ≤ numbers[i] ≤ 1,000,000 이기 때문에 0인 경우는 뒷 큰수가 없는 경우.
-            if (answer[i] == 0) {
-                // 뒷 큰수가 없는 경우 -1로 저장
-                answer[i] = -1;
-            }
+            stack.push(i); //하강곡선일 때
         }
+
+        while (!stack.isEmpty()) {
+            answer[stack.pop()] = -1;
+        }
+
         return answer;
     }
 }
